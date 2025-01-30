68ba4b489e44cafd6bc8e7dea2d593f1f2dbadaf
Arash <arashsaleh798@gmail.com>
2025-01-30 13:06:40 -0800

    exercise 6 is complete

--- app.js
+++ app.js
@@ -281,3 +281,36 @@ console.log('Exercise 5 correct result: ', [
 ]);
 
 
+/*
+Exercise 6: Array.prototype.some()
+
+Determine if there is at least one person in the devs array
+ who is 18 years 
+old or older.
+
+- You have an array of people with their respective ages.
+- Use the Array.prototype.some() method to check if any person 
+in the array is 
+  18 years old or older.
+- Store the result (true or false) in the variable 'isAdultPresent'. 
+*/
+
+const isAdultPresent = devs.some((dev) => {
+    return 2025 - dev.year >= 18
+
+});
+
+// Complete the exercise in the space below:
+
+// Check your work:
+console.log('Exercise 6 my result: ', isAdultPresent);
+console.log('Exercise 6 correct result: ', true);
+
+
+// const devs = [
+//     { name: 'Alex', year: 1988 },
+//     { name: 'Dani', year: 1986 },
+//     { name: 'Matt', year: 1970 },
+//     { name: 'Wes', year: 2015 },
+//   ];
+  
